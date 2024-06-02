9afb03b00232a4db81e41a37cfb85434241eb2b2
rakugono <71830943+rakugono@users.noreply.github.com>
2024-06-02 13:59:32 +0900

    Delete nvim/lua directory

--- nvim/lua/mac.lua
+++
@@ -1,6 +0,0 @@
--- Insert, Commandを抜けるとime offにする(mac)
-vim.cmd [[
-    let s:imeoff = 'osascript -e "tell application \"System Events\" to key code 102"'
-    autocmd InsertLeave * :call system(s:imeoff)
-    autocmd CmdlineLeave * :call system(s:imeoff)
-    ]]
