2e35c68c98fb7acc5e3c3ad4d3e80c8f5d2ac8a8
rakugono <71830943+rakugono@users.noreply.github.com>
2024-06-02 12:56:07 +0900

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+require('base')
+require('hilights')
+require('keymap')
+require('plugins')
+
+
+-- osを判別する関数を作成
+local has = function(x)
+    return vim.fn.has(x) == 1
+end
+local is_mac = has "macunix"  -- is_macでmacOSを判別する
+local is_win = has "win32"  -- is_winでwinOSを判別する
+
+-- macOSの設定を読み込む
+if is_mac then
+    require('mac')
+end
+-- winOSの設定を読み込む
+if is_win then
+    require('win')  -- TODO: 動作チェックする
+end
