4e361765597fc3c5d1fa4ce2f21c898f1e6efd95
rakugono <71830943+rakugono@users.noreply.github.com>
2024-06-02 13:54:34 +0900

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 -- require('base')
 -- require('hilights')
-require('keymap')
+-- require('keymap')
 -- require('plugins')
 
 
@@ -69,5 +69,38 @@ vim.opt.ambiwidth = "double"
 
 -- terminalの色を豊かにする
 vim.opt.termguicolors = true
+
+
+
+
+--- ./lua/keymap.luaを読み込む ---
+
+local keymap = vim.keymap
+-- option: n = normal, nx = normal + visual
+
+-- <Leader>を<Space>キーに設定した
+vim.g.mapleader = " "
+
+-- Split window  -- 画面分割後した方を選択する
+keymap.set('n', '<Leader>-', ':split<Return><C-w>w', {silent = true})
+keymap.set('n', '<Leader>\\', ':vsplit<Return><C-w>w', {silent = true})
+-- Move window
+keymap.set('n', ',', '<C-w>w')
+
+-- 行の端に移動する
+keymap.set('n', '<Space>h', '^')
+keymap.set('n', '<Space>l', '$')
+
+-- <Esc>を2回押下してハイライトをやめる
+keymap.set('n', '<Esc><Esc>', ':<C-u>set nohlsearch<Return>', {silent = true})
+
+-- 表示行を移動する  -- 論理行をg付きで移動する
+keymap.set('n', 'k', 'gk')
+keymap.set('n', 'gk', 'k')
+keymap.set('n', 'j', 'gj')
+keymap.set('n', 'gj', 'j')
+
+
+
 
 
